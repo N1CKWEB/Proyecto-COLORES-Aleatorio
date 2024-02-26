786cd8896b7f6cd76da53ad15b6c5775f69ac5a3
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-02-26 13:01:37 -0300

    Realizando la función generar color hexadecimal aleatorio

--- Dinamica/app.js
+++ Dinamica/app.js
@@ -2,3 +2,18 @@
 const boton = document.querySelector('button');
 const color = document.getElementById('color');
 
+// Generar un color hexadecimal aleatorio.
+function generarColorHexAleatorio() {
+  let digitos = '0123456789ABCDEF'; // 16 opciones.
+  let color = '#'; // El color inicia con #.
+
+  for (let i = 0; i < 6; i++) {
+    // Escoger uno de los 16 digitos posibles.
+    let indiceAleatorio = Math.floor(Math.random() * 16);
+    // Agregarlo al codigo del color.
+    color += digitos[indiceAleatorio];
+  }
+  
+  return color;
+}
+
